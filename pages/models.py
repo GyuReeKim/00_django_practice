548ab80438ae2c087d627328133d489ba5e32844
GyuReeKim <starkim96@naver.com>
2019-10-04 17:47:20 +0900

    1004

--- pages/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Page(models.Model):
-    myname = models.CharField(max_length=50)
-    myage = models.IntegerField()
--- todos/admin.py
+++ todos/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Page
 
 # Register your models here.
-admin.site.register(Page)
